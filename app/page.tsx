76a73c1c8579ce4256aa66764b861019b759996e
mbilal71 <mbilal71@gmail.com>
2024-05-21 12:00:51 +0500

    padding update

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Menubar } from '@/components/ui/menubar'
 const ButtonComponent = () => {
   return (
     <div className="bg-blue-400">
-    <Menubar className="p-20 justify-around">
+    <Menubar className="p-3 justify-around">
     <Button className="bg-slate-100 rounded">Claculator</Button>
     <Button className="bg-slate-100">Todo App</Button>
     <Button className="bg-slate-100">Unit Converter</Button>
